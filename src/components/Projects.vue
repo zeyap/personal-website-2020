ed8cd90116071b23e79205d11cf43056e3eb317a
zeyap <zp82@cornell.edu>
2020-02-27 22:00:41 -0500

    added teddy project

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -17,9 +17,8 @@
 
     <div id="section-me">
       <div class="headmargin">&nbsp</div>
-      <p style="margin-bottom:0">Frontend Engineer passionate about creating friendly, pixel perfect & performant user interfaces</p>
-      <p style="margin:0">2nd year Master student at Cornell Information Science</p>
-      <p style="margin-top:0">Spent 18 years growing up in Changsha, China and went on for university at Hangzhou</p>
+      <p style="margin-bottom:0">Frontend Engineer based in NYC, passionate about creating friendly, pixel perfect & performant user interfaces</p>
+      <p style="margin-top:0">Born and raised in Changsha, China and went on for university at Hangzhou & Ithaca NY</p>
       
       <div>
         <a class="underline contact" href="mailto:zp82 @ cornell.edu">Email</a> 
@@ -30,11 +29,10 @@
     <div id="section-projects">
       <div v-for="(project,key) in [...projects, ...other]" v-bind:key="'project-card'+key">
         <div>
-          <h2 class="h2-small" style="color: #904e95">{{project.name}}</h2>
+          <a target="_blank" v-bind:href="project.link"><h2 class="h2-small" style="color: #904e95">{{project.name}}</h2></a>
           <div><span class="tag" v-for="(tag, i) in project.tags" v-bind:key="'projtag'+i">{{tag}}</span></div>
         </div>
-        
-        <a target="_blank" v-bind:href="project.link"><img v-bind:src="project.img" class="project-picture-img shadowed"/></a>
+        <img v-if="project.img" v-bind:src="project.img" class="project-picture-img shadowed"/>
       </div>
     </div>
 
